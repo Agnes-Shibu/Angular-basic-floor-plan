e30a277e7d77fab0bb302e391ed4d1b48e46f64d
agnes <agnesjoyal11@gmail.com>
2024-02-07 23:26:14 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BathroomComponent } from './components/bathroom/bathroom.component';
+import { BedroomComponent } from './components/bedroom/bedroom.component';
+import { DiningroomComponent } from './components/diningroom/diningroom.component';
+import { KitchenComponent } from './components/kitchen/kitchen.component';
+import { HallComponent } from './components/hall/hall.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BathroomComponent, BedroomComponent, DiningroomComponent, KitchenComponent,HallComponent ,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fisat';
+  name : string = 'joyal';
+  email : string = "";
+  test(){
+    alert("my name is joyal");
+  }
+
 }
---
+++ src/app/components/diningroom/diningroom.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diningroom',
+  standalone: true,
+  imports: [],
+  templateUrl: './diningroom.component.html',
+  styleUrl: './diningroom.component.css'
+})
+export class DiningroomComponent {
+
+}
